fdb5106ed8a272c038c33a86d9e5126ff945f6fd
Deepak-thakur-321 <deepakthakur17756@gmail.com>
2025-09-11 19:44:08 +0530

    Implement AI Service with Socket integration

--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -1,22 +1,53 @@
 const { Server } = require("socket.io")
+const cookie = require("cookie")
+const jwt = require("jsonwebtoken")
+const aiService = require("../services/ai.service")
 
-async function socketServer(httpServer) {
-
+function initSocket(httpServer) {
    const io = new Server(httpServer)
-   io.on("connection", (socket) => {
-      console.log("Client Connected", socket.id)
 
-      socket.on("disconnect", (data) => {
-         console.log("A user Disconnect", data)
-      })
 
-      io.emit("message", "Hello All Client")
-      io.on("disconnection", (socket) => {
-         console.log("Client Disconnected", socket.id)
-      })
+   io.use((socket, next) => {
+
+      const cookies = socket.handshake.headers.cookie
+
+      const { token } = cookies ? cookie.parse(cookies) : {}
+
+      if (!token) {
+         return next(new Error("Authentication error"))
+      }
+
+      try {
+         const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+         socket.user = decoded
+
+         next()
+      } catch (err) {
+         return next(new Error("Invalid token"))
+      }
 
    })
+
+   io.on("connection", (socket) => {
+      console.log("A user connected")
+      console.log(socket.user)
+
+      socket.on("ai-message", async (message) => {
+         try {
+            await aiService.generateStream(message, (textChunk) => {
+               socket.emit("ai-response", textChunk);
+            });
+         } catch (err) {
+            console.error("AI stream error:", err);
+            socket.emit("ai-error", "Something went wrong while generating response.");
+         }
+      });
+
+      socket.on("disconnect", () => {
+         console.log("A user disconnected");
+      });
+   });
 }
 
-module.exports = socketServer
-
+module.exports = initSocket;
